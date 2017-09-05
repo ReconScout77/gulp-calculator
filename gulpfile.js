e3a9b82cb8e1284c9b6c9eaca71d3a00c0889d14
Michael Woldemedihin and Robert Murray <michael@>
2017-09-05 10:52:05 -0700

    add a jshint module and task to that help us in debugging

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,13 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var buildProduction = utilities.env.production;
+var jshint = require('gulp-jshint');
 
+gulp.task('jshint', function () {
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
 
 gulp.task('concatInterface', function(){
   return gulp.src(['./js/*-interface.js', './js/signup-interface.js'])
--- js/calculator.js
+++ js/calculator.js
@@ -4,6 +4,6 @@ function Calculator() {
 
 Calculator.prototype.Add = function (a, b) {
   return a + b;
-}
+};
 
 exports.calculatorModule = Calculator;
